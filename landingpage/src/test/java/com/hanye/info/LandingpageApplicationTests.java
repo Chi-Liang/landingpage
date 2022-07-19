6a27d01b0734b877b138d5d1065e9bcb264ae0cc
klchib3 <klchib3@gmail.com>
2022-07-19 23:25:23 +0800

    landingpage

--- landingpage/src/test/java/com/hanye/info/LandingpageApplicationTests.java
+++ landingpage/src/test/java/com/hanye/info/LandingpageApplicationTests.java
@@ -1,5 +1,7 @@
 package com.hanye.info;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.hanye.info.model.mongo.GroceryItem;
 import com.hanye.info.vo.ReturnSendMailVo;
 
 @SpringBootTest
@@ -29,6 +33,14 @@ public class LandingpageApplicationTests extends AbstractTest {
 
 		ReturnSendMailVo returnSendMailVo = mailJobService.startMailJob();
 		Assert.assertSame(returnSendMailVo.getResult(),"success");
+
+	}
+	
+	@Test
+	public void testMongo() {
+
+		List<GroceryItem> item = mongoTemplate.findAll(GroceryItem.class);
+		Assert.assertTrue(item.size() > 0);
 
 	}
 	
