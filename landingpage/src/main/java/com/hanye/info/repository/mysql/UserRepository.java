eeae43a8a332ffc2e3c8ee8dae89d5ff75bd51f4
klchib3 <klchib3@gmail.com>
2022-07-19 23:07:51 +0800

    ladningpage

--- landingpage/src/main/java/com/hanye/info/repository/mysql/UserRepository.java
+++ landingpage/src/main/java/com/hanye/info/repository/mysql/UserRepository.java
@@ -1,10 +1,10 @@
 package com.hanye.info.repository.mysql;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.hanye.info.model.mysql.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
